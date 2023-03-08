1924b6692fd3d42635976b82fdb5ed207d417b04
Abdulqadir <abdulqadir.b8604@gmail.com>
2023-03-09 01:23:43 +0530

    finally done 99%

--- script.js
+++ script.js
@@ -27,4 +27,48 @@ function startCounter(element, start, target) {
   startCounter(perfect_bodies, 3900, 4560);
   startCounter(working_hours, 100, 570);
   startCounter(success_stories, 452, 900);
+
+  const form = document.querySelector('#appointment-form');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Get form input values
+    const name = form.elements['name'].value;
+    const email = form.elements['email'].value;
+    const date = form.elements['date'].value;
+    const time = form.elements['time'].value;
+    const message = form.elements['message'].value;
+
+    // Send appointment data to server (example using fetch API)
+    fetch('/appointments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        date,
+        time,
+        message
+      })
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      // Show success message
+      const successMsg = document.querySelector('#success-message');
+      successMsg.style.display = 'block';
+    })
+    .catch(error => {
+      console.error('There was a problem with the appointment request:', error);
+      // Show error message
+      const errorMsg = document.querySelector('#error-message');
+      errorMsg.style.display = 'block';
+    });
+  });
+
+
   
