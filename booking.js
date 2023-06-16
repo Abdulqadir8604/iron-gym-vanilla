fcd1d50d2ac633e93f000b6105053af93060e293
Abdulqadir <76737223+Abdulqadir8604@users.noreply.github.com>
2023-06-17 03:15:23 +0530

    Update booking.js

--- booking.js
+++ booking.js
@@ -11,7 +11,7 @@ function submit(event) {
   const message = document.getElementById('message').value;
 
   // Make a POST request to the server to send the form data
-  fetch('https://irongymemailserver.abdulqadir8604.repl.co/send-email', {
+  fetch('http://irongymemailserver.abdulqadir8604.repl.co/send-email', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
